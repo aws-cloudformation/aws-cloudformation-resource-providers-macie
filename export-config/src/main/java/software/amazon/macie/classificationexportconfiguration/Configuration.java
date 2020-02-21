f03110ef58b8bfdca383a892ee1911d4370408f1
Erin Ward 🍕 <warderin@amazon.com>
2020-02-21 10:22:58 -0800

    Create directories and files for resources

---
+++ export-config/src/main/java/software/amazon/macie/classificationexportconfiguration/Configuration.java
@@ -0,0 +1,12 @@
+package software.amazon.macie.classificationexportconfiguration;
+
+import java.util.Map;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("aws-macie-classificationexportconfiguration.json");
+    }
+}
---
+++ job/src/main/java/software/amazon/macie/classificationjob/CallbackContext.java
@@ -0,0 +1,12 @@
+package software.amazon.macie.classificationjob;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Builder
+public class CallbackContext {
+
+}
