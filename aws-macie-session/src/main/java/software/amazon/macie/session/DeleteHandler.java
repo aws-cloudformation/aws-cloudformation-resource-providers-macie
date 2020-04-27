e27510573564815fb30ade1c1597bf597fae110f
Chandrakant Dhome <dhomecd@amazon.com>
2020-04-27 10:49:47 -0700

    Organize imports, remove custom endpoint

--- aws-macie-session/src/main/java/software/amazon/macie/session/DeleteHandler.java
+++ aws-macie-session/src/main/java/software/amazon/macie/session/DeleteHandler.java
@@ -6,7 +6,6 @@ import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
 import software.amazon.cloudformation.proxy.Logger;
 import software.amazon.cloudformation.proxy.OperationStatus;
 import software.amazon.cloudformation.proxy.ProgressEvent;
-import software.amazon.cloudformation.proxy.ProgressEvent.ProgressEventBuilder;
 import software.amazon.cloudformation.proxy.ProxyClient;
 import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
 
