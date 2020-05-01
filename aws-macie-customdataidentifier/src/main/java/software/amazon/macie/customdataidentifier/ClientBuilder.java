11e1deac9c195b734c819c4dd962019a0dd28079
Haitham Gad <haithamgad@gmail.com>
2020-05-01 14:36:10 -0700

    Fix schema & Macie endpoint (#22)
    Co-authored-by: Haitham Gad <haithamg@amazon.com>

--- aws-macie-customdataidentifier/src/main/java/software/amazon/macie/customdataidentifier/ClientBuilder.java
+++ aws-macie-customdataidentifier/src/main/java/software/amazon/macie/customdataidentifier/ClientBuilder.java
@@ -3,14 +3,9 @@ package software.amazon.macie.customdataidentifier;
 import software.amazon.awssdk.services.macie2.Macie2Client;
 import software.amazon.cloudformation.LambdaWrapper;
 
-import java.net.URI;
-
 public class ClientBuilder {
   public static Macie2Client getClient() {
-    // TODO remove
-    URI override = URI.create("https://macie2.prod.us-east-1.macie.amazonaws.com");
     return Macie2Client.builder()
-                       .endpointOverride(override)
                        .httpClient(LambdaWrapper.HTTP_CLIENT)
                        .build();
   }
